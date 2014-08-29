4c65b455fde29d42c3d71c77b6c273a21a84ad33
Braeden Robak <braedenrobak@gmail.com>
2014-08-29 16:06:17 -0600

    Added the Enviroment class skeleton, may not need it.

---
+++ Environment.py
@@ -0,0 +1,9 @@
+#This may be able to be kept as a Sprite since it doesn't do much special
+class Environment(pygame.sprite.Sprite):
+	images = []
+
+	def __init__(self,pos):
+		pygame.sprite.Sprite.__init__(self,self.containers)
+		self.image = self.images[0]
+		self.rect = self.image.get_rect()
+
