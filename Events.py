116d1e2a962d0a7111285590303afc2506f64b1b
Joshua Timmermans <josh@beardguy.ca>
2014-08-31 15:06:00 -0600

    All of your spacing is in tabs. Makes it scary well have to sort it out.

--- Events.py
+++ Events.py
@@ -1,16 +1,16 @@
 import pygame
-import constant
+import Constants
+import Player
 
-class Events(#All game objects
-								):
+class Events(#All game objects):
 
-	def player_moves(direction):
-	#Performs all action need to create player movement
-	#input@ direction indicated by keyboard state
-		player.move_horizontal(direction)
+    def player_moves(direction):
+    #Performs all action need to create player movement
+    #input@ direction indicated by keyboard state
+     player.move_horizontal(direction)
 
-	def player_jumps(delta_time):
-	#Calculate delta x/y for current jump
-	#Input@ time between last call
-		player.delta_xy -= player.gravity * delta_time
-		player.move_vertical(delta_time)
+    def player_jumps(delta_time):
+    #Calculate delta x/y for current jump
+    #Input@ time between last call
+    player.delta_xy -= player.gravity * delta_time
+    player.move_vertical(delta_time)
--- Player.py
+++ Player.py
@@ -1,49 +1,45 @@
 import pygame
-import constants
+import Constants
+
 
 class Player(pygame.sprite.Sprite):
-	base_speed = BASESPEED
-	speed_modifier = SPEEDMODIFIER
-	climb_speed = CLIMBSPEED
-	jump_force = JUMPFORCE
-	delta_xy = 0
-	gravity = BASEGRAVITY
-	hasJumped = False
-	curr_ground_y = BASE_GROUND_Y
-	images = []
-	
-	def __init__(self):
-		pygame.sprite.Sprite(self, self.containers)
-		self.image = self.images[0]
-		self.rect = self.image.rect.get_rect(midbottom-SCREENRECT.midbottom)
-	
-	def move_horizontal(self,direction):
-	# Moves character based on the given direction
-		if direction > 0:
-			self.image = images[PLAYER_RIGHT_IMAGE]
-		else
-			self.image = images[PLAYER_LEFT_IMAGE]
+    base_speed = BASESPEED
+    speed_modifier = SPEEDMODIFIER
+    climb_speed = CLIMBSPEED
+    jump_force = JUMPFORCE
+    delta_xy = 0
+    gravity = BASEGRAVITY
+    hasJumped = False
+    curr_ground_y = BASE_GROUND_Y
+    images = []
 
-		self.rect.move_ip(base_speed*direction,0)
-		self.rect.clamp(SCREENRECT)
+    def __init__(self):
+        pygame.sprite.Sprite(self, self.containers)
+        self.image = self.images[0]
+        self.rect = self.image.rect.get_rect(midbottom - SCREENRECT.midbottom)
 
-	def move_vertical(self,delta_time):
-	# Moves character by the given delta x/y coordinates
-		self.rect.move_ip(0,sel.rect.y + (delta_xy*delta_time))
-		self.rect.clamp(SCREENRECT)
+    def move_horizontal(self, direction):
+        # Moves character based on the given direction
+        if direction > 0:
+            self.image = images[PLAYER_RIGHT_IMAGE]
+        else
+            self.image = images[PLAYER_LEFT_IMAGE]
 
-		if self.rect.y <= curr_ground_y
-			self.rect.move_ip(0,curr_ground_y)
-			delta_xy = 0
+        self.rect.move_ip(base_speed * direction, 0)
+        self.rect.clamp(SCREENRECT)
 
-	def jump():
-		hasJumped = True
+    def move_vertical(self, delta_time):
+        # Moves character by the given delta x/y coordinates
+        self.rect.move_ip(0, sel.rect.y + (delta_xy * delta_time))
+        self.rect.clamp(SCREENRECT)
 
-	def isJumping():
-		return hasJumped
+        if self.rect.y <= curr_ground_y
+            self.rect.move_ip(0, curr_ground_y)
+            delta_xy = 0
 
+    def jump():
+        hasJumped = True
 
+    def isJumping():
+        return hasJumped
 
-
-	
-	
