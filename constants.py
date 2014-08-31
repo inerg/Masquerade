15a991937e9700f77b50806ec11e5a3167667bd0
Joshua Timmermans <josh@beardguy.ca>
2014-08-31 15:31:10 -0600

    Fixed what need to be imported to properly creat the Rectangle.

--- constants.py
+++ constants.py
@@ -1,7 +1,9 @@
+from pygame.rect import Rect
+
 BASESPEED = 10
 SPEEDMODIFIER = 2
 JUMPFORCE = 15
 CLIMBSPEED = 10
 GRAVITY = 9.81
-SCREENRECT = rect(0,0,680,480)
+SCREENRECT = Rect(0,0,680,480) #We should fully figure out how all these values work as its not clear in pygame documentation
 
