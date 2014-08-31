b8e6d59dbf2ad80580c684407d0f6ae7c2ad9bd9
Joshua Timmermans <josh@beardguy.ca>
2014-08-31 17:29:21 -0600

    Fixed anything that I may have done that would have broken stuff. Apparently you need images so what i was trying didnt work /rage quit for the day.

--- Player.py
+++ Player.py
@@ -1,22 +1,22 @@
 import pygame
-import Constants
+import constants
 
 
 class Player(pygame.sprite.Sprite):
-    base_speed = Constants.BASESPEED
-    speed_modifier = Constants.SPEEDMODIFIER
-    climb_speed = Constants.CLIMBSPEED
-    jump_force = Constants.JUMPFORCE
+    base_speed = constants.BASESPEED
+    speed_modifier = constants.SPEEDMODIFIER
+    climb_speed = constants.CLIMBSPEED
+    jump_force = constants.JUMPFORCE
     delta_xy = 0
-    gravity = Constants.BASEGRAVITY
+    gravity = constants.GRAVITY
     hasJumped = False
-    curr_ground_y = Constants.BASE_GROUND_Y
+    curr_ground_y = constants.BASE_GROUND_Y
     images = []
 
     def __init__(self):
-        pygame.sprite.Sprite(self, self.containers)
+        pygame.sprite.Sprite.__init__(self, self.containers)
         self.image = self.images[0]
-        self.rect = self.image.rect.get_rect(midbottom - SCREENRECT.midbottom)
+        self.rect = self.image.rect.get_rect(midbottom - constants.SCREENRECT.midbottom)
 
     def move_horizontal(self, direction):
         # Moves character based on the given direction
@@ -25,21 +25,24 @@ class Player(pygame.sprite.Sprite):
         else
             self.image = images[PLAYER_LEFT_IMAGE]
 
-        self.rect.move_ip(base_speed * direction, 0)
-        self.rect.clamp(SCREENRECT)
+    self.rect.move_ip(base_speed * direction, 0)
+    self.rect.clamp(constants.SCREENRECT)
 
-    def move_vertical(self, delta_time):
-        # Moves character by the given delta x/y coordinates
-        self.rect.move_ip(0, sel.rect.y + (delta_xy * delta_time))
-        self.rect.clamp(SCREENRECT)
 
-        if self.rect.y <= curr_ground_y
-            self.rect.move_ip(0, curr_ground_y)
-            delta_xy = 0
+def move_vertical(self, delta_time):
+    # Moves character by the given delta x/y coordinates
+    self.rect.move_ip(0, sel.rect.y + (delta_xy * delta_time))
+    self.rect.clamp(constants.SCREENRECT)
 
-    def jump():
-        hasJumped = True
+    if self.rect.y <= curr_ground_y
+        self.rect.move_ip(0, curr_ground_y)
+        delta_xy = 0
 
-    def isJumping():
-        return hasJumped
+
+def jump():
+    hasJumped = True  # while not important right now I've read use 1 and 0 is faster for execution although minorly for true false checks
+
+
+def isJumping():
+    return hasJumped
 
--- constants.py
+++ constants.py
@@ -7,3 +7,10 @@ CLIMBSPEED = 10
 GRAVITY = 9.81
 SCREENRECT = Rect(0,0,680,480) #We should fully figure out how all these values work as its not clear in pygame documentation
 
+#Colour constants since we have no images right now
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
