84fbc71c499d19fb9a8918fec212cfcad91b3065
Braeden Robak <braedenrobak@gmail.com>
2014-08-30 21:59:34 -0600

    Added some commenting to Player

--- Events.py
+++ Events.py
@@ -6,9 +6,11 @@ class Events(#All game objects
 
 	def player_moves(direction):
 	#Performs all action need to create player movement
+	#input@ direction indicated by keyboard state
 		player.move_horizontal(direction)
 
 	def player_jumps(delta_time):
 	#Calculate delta x/y for current jump
+	#Input@ time between last call
 		player.delta_xy -= player.gravity * delta_time
 		player.move_vertical(delta_time)
