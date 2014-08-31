de06fe34290b3fac565dbb7c49582fbc5bb3431f
Joshua Timmermans <josh@beardguy.ca>
2014-08-31 15:32:49 -0600

    Fixed the comment and player refrences.

--- Events.py
+++ Events.py
@@ -1,16 +1,16 @@
 import pygame
-import Constants
+import constants
 import Player
 
-class Events(#All game objects):
+class Events('''All game objects'''):
 
     def player_moves(direction):
     #Performs all action need to create player movement
     #input@ direction indicated by keyboard state
-     player.move_horizontal(direction)
+     Player.move_horizontal(direction)
 
     def player_jumps(delta_time):
     #Calculate delta x/y for current jump
     #Input@ time between last call
-    player.delta_xy -= player.gravity * delta_time
-    player.move_vertical(delta_time)
+    Player.delta_xy -= Player.gravity * delta_time
+    Player.move_vertical(delta_time)
