94ea772ce6f3aef9a1fb3230ff5acebdd4f629ed
Braeden Robak <braedenrobak@gmail.com>
2014-08-30 18:26:20 -0600

    Minor overhaul of object design and added a new Events class to better the modularity(?) of design.

---
+++ Events.py
@@ -0,0 +1,11 @@
+import pygame
+import constant
+
+class Events(#All game objects
+								):
+
+	def player_moves():
+	#Determines which direction by keyboard input
+
+	def player_jumps(delta_time):
+	#Calculate delta x/y for current jump
--- Player.py
+++ Player.py
@@ -14,12 +14,21 @@ class Player(pygame.sprite.Sprite):
 		self.image = self.images[0]
 		self.rect = self.image.rect.get_rect(midbottom-SCREENRECT.midbottom)
 	
-	def move(self,direction):
+	def move_horizontal(self,direction):
 	# Moves character based on the given direction
+		if direction > 0:
+			self.image = images[PLAYER_RIGHT_IMAGE]
+		else
+			self.image = images[PLAYER_LEFT_IMAGE]
 
-	def jump(self):
-	# Moves character in uniform arch, based on gravity
-	# Allow movement in air?
-	# Can't remeber the formula for that stuff
+		self.rect.move_ip(base_speed*direction,0)
+		self.rect.clamp(SCREENRECT)
+
+	def move_vertical(self,delta_x,delta_y):
+	# Moves character by the given delta x/y coordinates
+		self.rect.move_ip(delta_x,delta_y)
+		self.rect.clamp(SCREENRECT)
+
+
 	
 	
